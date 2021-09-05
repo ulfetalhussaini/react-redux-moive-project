83dc8a7d177ebc59a52b42938543a484bc32b350
lana <lana.km98@gmail.com>
2021-09-05 15:23:20 +0300

    Add genres to card movie

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
 
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Card, Button, Badge } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGenres } from '../redux/fetchGenres';
 
 function MovieCard({ movie }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGenres());
+  }, [fetchGenres]);
+
+  const genres = useSelector((state) => state.genres.genres);
+
   return (
     <Card style={{ width: '20%' }}>
       <Card.Img
@@ -14,6 +24,19 @@ function MovieCard({ movie }) {
         <Card.Title>{movie.original_title}</Card.Title>
         <Card.Text>Released at {movie.release_date}</Card.Text>
         <Card.Text>Rating: {movie.vote_average}</Card.Text>
+        <Card.Text>
+          {movie.genre_ids.map((id) =>
+            genres
+              .filter((genre) => genre.id === id)
+              .map((genre) => (
+                <span key={genre.id}>
+                  <Badge bg="warning" text="dark">
+                    {genre.name}
+                  </Badge>{' '}
+                </span>
+              ))
+          )}
+        </Card.Text>
         <Button variant="primary">Bookmark</Button>
       </Card.Body>
     </Card>
