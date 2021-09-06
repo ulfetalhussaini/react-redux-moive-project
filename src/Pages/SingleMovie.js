9055980c7024dd29fc875b8dc5495655565ea3e7
lana <lana.km98@gmail.com>
2021-09-06 23:37:42 +0300

    Add single movie detail

--- src/Pages/SingleMovie.js
+++ src/Pages/SingleMovie.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Badge } from 'react-bootstrap';
 
 function SingleMovie() {
   const movie = useSelector((state) => state.singleMovie.singleMovie);
@@ -11,8 +11,70 @@ function SingleMovie() {
         <Col style={{ padding: '50px' }}>
           <h2>{movie.original_title}</h2>
           <p>{movie.overview}</p>
-          <p>Rating: {movie.vote_average}</p>
-          <p>Budget: {movie.budget}</p>
+          <p>
+            <b>Genre:</b>{' '}
+            {movie.genres.map((genre) => (
+              <span key={genre.id}>
+                <Badge bg="warning" text="dark">
+                  {genre.name}
+                </Badge>{' '}
+              </span>
+            ))}
+          </p>
+          <p>
+            <b>Released at:</b> {movie.release_date}
+          </p>
+          <p>
+            <b>Rating:</b> {movie.vote_average}
+          </p>
+          <p>
+            <b>Duration:</b> {movie.runtime} min
+          </p>
+          <p>
+            <b>Language:</b>{' '}
+            {movie.spoken_languages.map((language) => (
+              <span key={language.english_name}>
+                <Badge bg="warning" text="dark">
+                  {language.english_name}
+                </Badge>{' '}
+              </span>
+            ))}
+          </p>
+          <p>
+            <b>Budget:</b> {movie.budget}
+          </p>
+          <p>
+            <b>Revenue:</b> {movie.revenue}
+          </p>
+          <p>
+            <b>Popularity:</b> {movie.popularity}
+          </p>
+          <p>
+            <b>Vote Average:</b> {movie.vote_average}
+          </p>
+          <p>
+            <b>Vote Count:</b> {movie.vote_count}
+          </p>
+          <p>
+            <b>Production Company:</b>{' '}
+            {movie.production_companies.map((company) => (
+              <span key={company.id}>
+                <Badge bg="warning" text="dark">
+                  {company.name}
+                </Badge>{' '}
+              </span>
+            ))}
+          </p>
+          <p>
+            <b>Production Country:</b>{' '}
+            {movie.production_countries.map((country) => (
+              <span key={country.name}>
+                <Badge bg="warning" text="dark">
+                  {country.name}
+                </Badge>{' '}
+              </span>
+            ))}
+          </p>
         </Col>
         <Col>
           <img
