37f8e5d9ca00d60d524e54851992e89b27b91af2
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-09-06 19:49:52 +0300

    update style

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -5,30 +5,30 @@ function Footer() {
   return (
     <div
       style={{
-        padding: '50px',
+        //     padding: '50px',
         backgroundColor: '#f2f2f2',
         textAlign: 'center',
         color: 'black',
       }}
     >
-      <NavLink to="/" exact style={{ textDecoration: 'none', padding: '50px' }}>
+      <NavLink to="/" exact style={{ textDecoration: 'none', padding: '5px' }}>
         Home
       </NavLink>
       <NavLink
         to="/actors"
         exact
-        style={{ textDecoration: 'none', padding: '50px' }}
+        style={{ textDecoration: 'none', padding: '5px' }}
       >
         Actors
       </NavLink>
       <NavLink
         to="/movies"
         exact
-        style={{ textDecoration: 'none', padding: '50px' }}
+        style={{ textDecoration: 'none', padding: '5px' }}
       >
         Movies
       </NavLink>
-      <NavLink to="/about" style={{ textDecoration: 'none', padding: '50px' }}>
+      <NavLink to="/about" style={{ textDecoration: 'none', padding: '5px' }}>
         About
       </NavLink>
       <h5 style={{ padding: '50px' }}>Ulfet & Lana</h5>
