5ad323c70102113eeac9d5ff1e12c1b5eb128510
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-09-08 23:50:38 +0300

    install & test i18next

--- src/Components/PopularMovies.js
+++ src/Components/PopularMovies.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import { fetchPopularMovies } from '../redux/fetchReducer';
 import MovieCard from './MovieCard';
 
@@ -12,10 +13,14 @@ function PopularMovies() {
     dispatch(fetchPopularMovies());
   }, [fetchPopularMovies]);
 
+  const { t } = useTranslation();
+
   return (
     <div>
       <Container>
-        <h1 style={{ padding: '20px', textAlign: 'center' }}>Popular Movies</h1>
+        <h1 style={{ padding: '20px', textAlign: 'center' }}>
+          {t('Popular_Movies')}
+        </h1>
         <Row lg={4} md={2} sm={1}>
           {movies.map((movie) => (
             <Col>
